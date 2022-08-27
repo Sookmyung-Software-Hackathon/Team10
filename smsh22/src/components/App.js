39bfe9427e235d97b912a7abc119d7f098e9a9d3
eunxoo <dmstn7989@naver.com>
2022-08-27 12:40:58 +0900

    feat: 초기 세팅

---
+++ smsh22/src/components/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import AppRouter from "./Router";
+import { authService } from "../fbase";
+
+function App() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObj({
+          uid: user.uid,
+          displayName: user.displayName,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+        if (user.displayName === null) {
+          const name = user.email.split("@")[0];
+          user.displayName = name;
+        }
+      } else {
+        setIsLoggedIn(false);
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      uid: user.uid,
+      displayName: user.displayName,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
+  return (
+    <>
+      {init ? (
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={isLoggedIn}
+          userObj={userObj}
+        />
+      ) : (
+        "initializing..."
+      )}
+      <footer>&copy; {new Date().getFullYear()} Team10</footer>
+    </>
+  );
+}
+export default App;
