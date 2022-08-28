52cc1a3b09d7cc92d8b173e575307de0c9e97039
Nayujin <00nyj@sookmyung.ac.kr>
2022-08-28 11:59:32 +0900

    fix: 정보를 메인에서 전달해주기

--- smsh22/src/page/Home.js
+++ smsh22/src/page/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { ImLocation2 } from "react-icons/im";
 import { dbService } from "../fbase";
 import Each from "../components/Each";
+import Search from "../components/Search";
 // import { useNavigate } from "react-router-dom";
 
 const Home = () => {
@@ -23,6 +24,7 @@ const Home = () => {
     }));
       setList(listArray);
     })
+    console.log(list);
 },[]);
   const onBtnClick=(e)=>{
     dbService.doc(`left/${e}`).get()
@@ -85,6 +87,14 @@ const Home = () => {
         <Box>
           <Map>
             <MapImg src="/image/naver_map.png" alt="사진 출처: NAVER"></MapImg>
+            {/* {pinlist.map((each)=>(
+              <Search 
+                key={each.id}
+                name={each.name}
+                left={each.left}
+                top={each.top}
+              />
+            ))} */}
             <Soon onClick={()=>onBtnClick('soon')}>
               <ImLocation2 style={{fontSize:'4rem',focusable:true}} focusable={true}></ImLocation2>
             </Soon>
@@ -186,6 +196,22 @@ const Myung = styled.div`
     opacity: 1;
   }
 `;
+
+const pinlist=[
+  {id:'soon',left: 40, top:11},
+  {id:'myung',left: 30, top:19},
+  {id:'jin',left: 33, top:12},
+  {id:'sae',left: 31, top:24},
+  {id:'student',left: 43.5, top:23},
+  {id:'prime',left: 42.5, top:32},
+  {id:'flaza',left: 34, top:31},
+  {id:'science',left: 56, top:33},
+  {id:'library',left: 53, top:39},
+  {id:'art',left: 42, top:34},
+  {id:'music',left: 34.5, top:36.5},
+  {id:'medi',left: 39, top:42},
+
+];
 const Soon = styled(Myung)`
   left: 40em;
   top: 11em;
